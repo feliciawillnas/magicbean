ceaf0819885dfded179ab57217b9452660571603
felicia <feliciawillnas@gmail.com>
2023-08-20 18:32:12 +0200

    button responsive

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,4 +1,30 @@
+import React, { useEffect, useState } from "react";
+
 export default function ReadMoreButton() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const readMoreButton = {
+    backgroundColor: "black",
+    fontFamily: "Lacquer",
+    color: "#a5310c",
+    padding: "0.6rem",
+    borderRadius: "50px",
+    border: "none",
+    fontSize: isMobile ? "1.8rem" : "2.5rem",
+  };
+
   return (
     <>
       <button style={readMoreButton}>read more</button>
@@ -13,16 +39,6 @@ function AddToCartButton() {
     </>
   );
 }
-
-const readMoreButton = {
-  backgroundColor: "black",
-  fontFamily: "Lacquer",
-  color: "#a5310c",
-  padding: "0.6rem",
-  borderRadius: "50px",
-  border: "none",
-  fontSize: "2.5rem",
-};
 
 const addToCartButton = {
   backgroundColor: "black",
