6f01cb28fb00e069aaed48161aedad9a0d46d746
felicia <feliciawillnas@gmail.com>
2023-08-20 18:29:02 +0200

    main responsive

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,26 +1,41 @@
+import React, { useEffect, useState } from "react";
 import ReadMoreButton from "./button";
 import Card from "./card";
 
 function InformationSection() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <>
-      <section style={sectionStyle}>
-        <div style={divStyleLeft}>
-          <h2 style={informationSectionh2}>from bean to cup</h2>
-          <p style={pStyle}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
-            facere modi ex sint, praesentium laborum nobis quia repellat nemo ut
-            ab quae aspernatur eius explicabo iste expedita tenetur mollitia
-            corrupti! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Magnam, asperiores.
-          </p>
-          <ReadMoreButton />
-        </div>
-        <div style={divStyleRight}>
-          <img style={imgStyle} src="src/assets/coffeeplant3.png" alt="" />
-        </div>
-      </section>
-    </>
+    <section style={sectionStyle}>
+      <div style={{ ...divStyleLeft, flex: isMobile ? "1" : "none" }}>
+        <h2 style={informationSectionh2}>from bean to cup</h2>
+        <p style={pStyle}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae facere
+          modi ex sint, praesentium laborum nobis quia repellat nemo ut ab quae
+          aspernatur eius explicabo iste expedita tenetur mollitia corrupti!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam,
+          asperiores.
+        </p>
+        <ReadMoreButton />
+      </div>
+      <div>
+        {isMobile ? null : (
+          <img style={imgStyle} src="src/assets/coffeeplant3.jpg" alt="" />
+        )}
+      </div>
+    </section>
   );
 }
 
@@ -50,12 +65,10 @@ const pStyle = {
   fontSize: "2rem",
 };
 
-const divStyleRight = {};
-
 const imgStyle = {
-  height: "400px",
-  width: "600px",
-  objectFit: "cover",
+  height: "450px",
+  width: "100%",
+  objectFit: "contain",
   borderRadius: "5%",
 };
 
